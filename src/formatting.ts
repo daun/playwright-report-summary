ffb6078dec5db6ac0494e6a09c3b68b5e061c093
Philipp Daun <post@philippdaun.net>
2023-09-07 19:51:20 +0200

    Apply formatting

--- src/formatting.ts
+++ src/formatting.ts
@@ -1,4 +1,7 @@
-export function renderMarkdownTable(rows: string[][], headers: string[] = []): string {
+export function renderMarkdownTable(
+  rows: string[][],
+  headers: string[] = []
+): string {
   if (!rows.length) {
     return ''
   }

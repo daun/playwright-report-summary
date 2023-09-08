0b2f7c9e8a25c74e7546b68f17670bfa88caa35e
Philipp Daun <post@philippdaun.net>
2023-09-08 13:09:05 +0200

    Add icon rendering tests

--- __tests__/icons.test.ts
+++ __tests__/icons.test.ts
@@ -5,8 +5,17 @@
 import { expect } from '@jest/globals'
 import { renderIcon } from '../src/icons'
 
-describe('icons.ts', () => {
+describe('renderIcon', () => {
 	it('returns a string', async () => {
 		expect(renderIcon('something')).toBe('')
+	})
+	it('renders markdown images', async () => {
+		expect(renderIcon('passed')).toMatch(/^[!]\[([\w\s\d]+)\]\((https?:\/\/[\w\d./?&=_-]+)\)$/i)
+	})
+	it('colorizes markdown images', async () => {
+		expect(renderIcon('passed')).toMatch(/\?.*?\bcolor=[\w]{6}\b/i)
+	})
+	it('renders emojis', async () => {
+		expect(renderIcon('passed', { iconStyle: 'emojis' })).toBe('✅')
 	})
 })
